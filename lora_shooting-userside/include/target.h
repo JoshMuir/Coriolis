b034e6eaafb6de478463afd1a0d61d9d743af9e9
Josh <mimtutor@hotmail.com>
2025-08-16 12:13:46 +1000

    add default lora config and implement basics

--- lora_shooting-userside/include/target.h
+++ lora_shooting-userside/include/target.h
@@ -6,26 +6,45 @@
 #include <LoRa.h>
 
 typedef struct  {
-    int frequency;
+    long frequency;
     int bandwidth;
     int spreadingFactor;
     int codingRate;
+    byte syncWord;
     int power;
+    uint16_t preambleLength;
     int gain;
 } LoRaConfig;
+
+typedef struct{
+    uint16_t speed;
+    bool direction;
+} windData_t;
 
 
 class Target {
 public:
     Target(int address, LoRaConfig config);
+    Target(int address); // use default config
     void begin();
-    bool sendMessage(const String &message);
     bool receiveMessage(String &message);
     void parseMessage(const String &message, String &senderAddress, String &messageType, String &payload);
+
+    bool getInitStatus();
+
+    // Sending messages 
+
+    bool sendMessage(windData_t windData);
+    
+    bool sendMessage(String &message);
 
 private:
     LoRaConfig _config;
     int _deviceAddress;
+    bool _isInitialized;
+    bool _busy;
+    bool _sendSuccess;
+
 };
 
 #endif
--- lora_shooting-userside/src/target.cpp
+++ lora_shooting-userside/src/target.cpp
@@ -18,8 +18,67 @@
     V1.0 : PoC Setup LoRa communication and send basic wind data
 */
 
+LoRaConfig defaultConfig = {
+    915E6,    // frequency
+    125E3,    // bandwidth
+    9,        // spreading factor
+    7,        // coding rate
+    0x12,     // sync word
+    10,       // power
+    8,         // preamble length
+    0       // gain
+};
 
-Target::Target(int address, LoRaConfig config) : _deviceAddress(address), _config(config) {
+
+
+Target::Target(int address, LoRaConfig config) : _deviceAddress(address), _config(config) 
+{
     // Constructor implementation
+    bool _isInitialized = false;
+    bool _busy = false;
+    bool _sendSuccess = false;
 }
 
+Target::Target(int address) : 
+_deviceAddress(address),
+_config(defaultConfig)
+{
+    _isInitialized = false;
+    _busy = false;
+    _sendSuccess = false;
+}
+
+void Target::begin()
+{
+    // Initialise LoRa module as per API 
+    LoRa.setSPI(SPI);
+    LoRa.setTxPower(_config.power);
+    LoRa.setSpreadingFactor(_config.spreadingFactor);
+    LoRa.setBandwidth(_config.bandwidth);
+    LoRa.setCodingRate4(_config.codingRate);
+    
+    _isInitialized = LoRa.begin(_config.frequency);
+    
+}
+
+bool Target::getInitStatus()
+{
+    return this->_isInitialized;
+}
+
+bool Target::sendMessage(windData_t windData)
+{
+    //while(LoRa.beginPacket() == 0) delay(100);
+    LoRa.beginPacket();
+    LoRa.write(windData.speed, sizeof(windData.speed));
+    LoRa.write(windData.direction, sizeof(windData.direction));
+    LoRa.endPacket();
+}
+
+bool Target::sendMessage(String &message)
+{
+    LoRa.beginPacket();
+    LoRa.print(message);
+    LoRa.endPacket();
+    return true;
+}
